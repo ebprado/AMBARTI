e2b3379b16614adbb99d27698c7cbebe2f3ddf5c
Estevão <estevao.prado.2019@mumail.ie>
2021-01-14 15:57:18 +0000

    Fix a simulation bug

--- 04_simulation/04_run_simulations.R
+++ 04_simulation/04_run_simulations.R
@@ -1,14 +1,14 @@
 library(devtools)
 install_github("ebprado/AMBARTI",
-               # ref = 'main',
+               ref = 'main',
                auth_token = '0fbb079f1b2c6c830725db8d82218ff58bde9f60')
 
 library(AMBARTI)
 
 save_file = "/Users/estevaoprado/Documents/GitHub/AMBARTI/04_simulation/"
 
-I = c(5, 15, 30) # Number of genotypes
-J = c(5, 15, 30) # Number of environments
+I = 10 # c(5, 15, 30) # Number of genotypes
+J = 10 # c(5, 15, 30) # Number of environments
 s_alpha = c(1, 5) # standard deviation of alpha
 s_beta = c(1, 5) # standard deviation of beta
 s_y = c(1, 5) # standard deviation of y
@@ -58,7 +58,7 @@ for (i in 1:n_comb){
     nseed = nseed + 1
 
     print(paste('comb = ', i, ' out of ', n_comb , ' (rep = ', j, ')', sep=''))
-    
+
     # save files
     filename = paste('I', I,
                      'J', J,
@@ -68,16 +68,16 @@ for (i in 1:n_comb){
                      'L', gsub(', ', '', toString(lambda)),
                      'r', j,
                      sep='')
-    
+
     data_filename    = paste(filename, '_train_data.RData',     sep='')
     ammi_filename    = paste(filename, '_classical_AMMI.RData', sep='')
     bammi_filename   = paste(filename, '_bayesian_AMMI.RData',  sep='')
     ambarti_filename = paste(filename, '_AMBARTI.RData',        sep='')
-    
+
     save(data,           file = paste(save_file, data_filename,    sep=''))
     save(classical_AMMI, file = paste(save_file, ammi_filename,    sep=''))
     save(bayesian_AMMI,  file = paste(save_file, bammi_filename,   sep=''))
     save(ambarti,        file = paste(save_file, ambarti_filename, sep=''))
-    
+
   }
 }
