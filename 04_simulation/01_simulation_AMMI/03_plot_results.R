141141a18c11c4dc96921693d4b51ad8ea4fca9f
Estevão <estevao.prado.2019@mumail.ie>
2021-02-02 16:15:36 +0000

    Generate plots

--- 04_simulation/01_simulation_AMMI/03_plot_results.R
+++ 04_simulation/01_simulation_AMMI/03_plot_results.R
@@ -9,22 +9,21 @@ load(paste(save_file, '00_results_consolidated.RData', sep=''))
 
 # Some preprocessing
 tab = save_results
-tab$id = factor(tab$id, levels = c('classical AMMI', 'Bayesian AMMI (postproc)', 'Bayesian AMMI (NO postproc)', 'AMBARTI'),
-                        labels = c('AMMI', 'B-AMMI (postproc)', 'B-AMMI (no postproc)', 'AMBARTI'))
-
-tab$Q = factor(sapply(strsplit(as.character(tab$lambda), split = ' '), function(x) length(x)), levels = c('1','2','3'), labels = c('Q = 1', 'Q = 2', 'Q = 3'))
+tab$id = factor(tab$id, levels = c('AMBARTI', 'classical AMMI', 'Bayesian AMMI (NO postproc)', 'Bayesian AMMI (postproc)'),
+                        labels = c('AMBARTI', 'AMMI', 'B-AMMI (no PP)', 'B-AMMI (PP)'))
+tab$Q = factor(tab$Q, levels = c('1','2','3'), labels = c('Q = 1', 'Q = 2', 'Q = 3'))
 tab$I = factor(tab$I, levels=c('10'), labels=c('I = 10'))
 tab$J = factor(tab$J, levels=c('10'), labels=c('J = 10'))
 tab$lambda = as.factor(tab$lambda)
-tab$sa = as.factor(tab$sa)
-tab$sb = as.factor(tab$sb)
+tab$sa = factor(tab$sa, levels=c('1','5'), labels=c(expression(paste(sigma[alpha],' = 1')), expression(paste(sigma[alpha],' = 5'))))
+tab$sb = factor(tab$sb, levels=c('1','5'), labels=c(expression(paste(sigma[beta],' = 1')), expression(paste(sigma[beta],' = 5'))))
 tab$sy = as.factor(tab$sy)
 
 # Generate plots
 save_plots = "/Users/estevaoprado/Documents/GitHub/AMBARTI/04_simulation/01_simulation_AMMI/"
 myplot <- function(varA, varB, varC){
 
-  # pdf(paste(save_plots, varB, varA, '.pdf', sep=''), width = 8, height = 6)
+  pdf(paste(save_plots, varB, varA, '.pdf', sep=''), width = 8, height = 6)
   if (varA == 'sa'){aux = expression(sigma[alpha])}
   if (varA == 'sb'){aux = expression(sigma[beta])}
 
@@ -46,32 +45,35 @@ myplot <- function(varA, varB, varC){
       title = varC,
       colour = '',
       y = aux_y) +
+    scale_x_discrete(labels = parse(text = levels(tab$sa))) +
     theme_bw(base_size = 12) +
     theme(plot.title = element_text(size = 15, hjust = 0.5),
+          strip.text.y = element_text(angle = 0),
           legend.position = 'bottom',
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank()) +
-    facet_wrap(~ Q, scales='free_x', nrow=1) +
-    # facet_grid(I ~ Q) +
+    # facet_wrap(~ Q, scales='free_x', nrow=1) +
+    # facet_wrap(Q ~ sb, scales='free_x', nrow=2) +
+    facet_grid(sb ~ Q, labeller = labeller(sb = label_parsed, Q=label_value)) +
     labs(colour='') +
     guides(col = guide_legend(nrow=2))
 
   print(xxx)
 
-  # dev.off()
+  dev.off()
 }
 
 myplot('sa','rrmse_alpha', 'RRMSE - alpha')
-myplot('sb','rrmse_beta',  'RRMSE - beta')
-myplot('sb','lambda_rrmse', 'RRMSE - lambda')
+myplot('sa','rrmse_beta',  'RRMSE - beta')
+myplot('sa','lambda_rrmse', 'RRMSE - lambda')
 # myplot('sa','lambda_rrmse', 'RRMSE - lambda')
-myplot('sb','gamma_rrmse', 'RRMSE - gamma')
+myplot('sa','gamma_rrmse', 'RRMSE - gamma')
 # myplot('sa','gamma_rrmse', 'RRMSE - gamma')
-myplot('sb','delta_rrmse', 'RRMSE - delta')
+myplot('sa','delta_rrmse', 'RRMSE - delta')
 # myplot('sa','delta_rrmse', 'RRMSE - delta')
 myplot('sa','rmse_blinear', 'RMSE - Interaction')
 # myplot('sb','rmse_blinear', 'RMSE - Bilinear part')
-myplot('sb','y_test_rmse', 'RMSE - y test')
+myplot('sa','y_test_rmse', 'RMSE - y test')
 # myplot('sa','y_test_rmse', 'RMSE - y test')
 
 # Check -----------
